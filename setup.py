838428f7fb9e3f9d77c4cb9f1913087a71656683
Flávia <flavithalopes@hotmail.com>
2021-01-31 22:03:45 -0300

    Estrutura básica do projeto
    - readme.md
    - configurações do projeto

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Web Crawler RDP Teste - Selenium, webdriver, python, beautifulsoup',
+    author='FlaviaLopes',
+    license='Apache-2.0 License',
+)
